04c27bf12761f162bd4c2683c710be0a5d5734d5
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2022-02-04 01:11:07 -0300

    creating conection to socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
 import express, {Request, Response} from "express"
-import {Server, Socket} from 'socket.io';
+import {Server, Socket} from "socket.io";
+import http from "http";
+import cors from 'cors';
+require('dotenv').config();
 
 const io = new Server();
 const app = express();
-const http = require('http').Server(app);
+const server = http.createServer(app);
+const router = express.Router();
 
+app.use(cors({
+
+}));
 app.use(express.json());
 
-app.get('/teste', (req: Request, res: Response) => {
-    console.log("Parâmetros: ", req.params);
-    console.log("Query", req.query);
-    res.json({usuário: "Teste"})
-});
+// app.get('/', (req : Request, res: Response) => {
+//     res.send({})
+// });
+io.on('connect',(socket)=>{
+    console.log("hello world");
+})
+io.on('error', (error)=>{
+    console.log('Erro:', error)
+})
 
-io.on('connection', (socket: Socket) => {
-    console.log("Pegou")
-});
-
-http.listen(8001, function () {
-    console.log("Listening on 8001");
+router.get("/", (req, res) => {
+    res.send({response: "Server running"});
+})
+server.listen(process.env.PORT, () => {
+    console.log("Server Started!"+ ` on Port: ${process.env.PORT}`);
 });
